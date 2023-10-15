3c2e9d853228c2b6b6d39012b1970d0e164bcc06
ChArLeS <charleshoong97@gmail.com>
2023-10-15 22:08:25 +0800

    Initialize React Router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import { theme } from "./style/theme";
 import { ThemeUIProvider } from "theme-ui";
 import { Provider } from "react-redux";
 import store from "./utilities/redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import NotFoundPage from "./components/notFoundPage";
 
 function App() {
   return (
     <Provider store={store}>
       <ThemeUIProvider theme={theme}>
-        <div className="App">
-          <LoginPage />
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<LoginPage />} />
+              {/* <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} /> */}
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeUIProvider>
     </Provider>
   );
