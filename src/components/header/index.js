3c79aa2612f80e50149f0e3ceedc563351df6143
ChArLeS <charleshoong97@gmail.com>
2023-10-17 21:05:24 +0800

    Beauty 404 page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,13 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { Button, Flex, Link } from "theme-ui";
 import { logout } from "../../utilities/redux/slice/authenticationSlice";
 import { reset } from "../../utilities/redux/slice/toDoSlice";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const auth = useSelector((state) => state.authentication);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+  const textColor = ["/", "/login", "/dashboard", "/create"].includes(
+    location.pathname
+  )
+    ? "black"
+    : "white";
 
   return (
     <div
@@ -30,15 +36,19 @@ export default function Header() {
         }}
       >
         <Link
-          sx={{ fontSize: [15, 20], fontWeight: 800, color: "black" }}
+          sx={{ fontSize: [15, 20], fontWeight: 800, color: textColor }}
           href="/"
         >
           To Do Web
         </Link>
         {auth.token ? (
           <Flex sx={{ gap: [10, 20], fontSize: [15], alignItems: "center" }}>
-            <Link href="/">Home</Link>
-            <Link href="dashboard">Dashboard</Link>
+            <Link href="/" sx={{ color: textColor }}>
+              Home
+            </Link>
+            <Link href="dashboard" sx={{ color: textColor }}>
+              Dashboard
+            </Link>
             <Button
               onClick={() => {
                 dispatch(logout());
--- src/components/notFoundPage.js
+++ src/components/notFoundPage.js
@@ -1,3 +1,22 @@
+/** @jsxImportSource theme-ui */
+
+import { Flex, Heading } from "theme-ui";
+
 export default function NotFoundPage() {
-  return <>404 Not Found</>;
+  return (
+    <Flex
+      sx={{
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "url('/assets/svg/not-found-background.svg')",
+        backgroundSize: "repeat",
+      }}
+    >
+      <Heading as={"h1"} sx={{ fontSize: 30, color: "white" }}>
+        404 Not Found
+      </Heading>
+    </Flex>
+  );
 }
